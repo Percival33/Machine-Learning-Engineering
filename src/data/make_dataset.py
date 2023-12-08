3535a6f50c65401fa0e5107841746c3a7c48fb15
Marcin Jarczewski <marcin.jarc@gmail.com>
2023-12-08 22:50:49 +0100

    feat: add pre commit config

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -17,6 +17,7 @@ def main(input_filepath, output_filepath):
 
 
 if __name__ == '__main__':
+
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
