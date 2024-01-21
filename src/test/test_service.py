90f0589104dc3a6b5feeaf73d583c91c7dc569f3
Marcin Jarczewski <marcin.jarc@gmail.com>
2024-01-21 01:30:03 +0100

    test: add mocks

--- src/test/test_service.py
+++ src/test/test_service.py
@@ -1,35 +1,12 @@
-from fastapi import HTTPException
 from fastapi.testclient import TestClient
 
 from service import app, run_model_dependency, run_model_for_user
+import mocks
 
 client = TestClient(app)
 
-
-async def override_dependency(model_type: str):
-    class OutputMock:
-        stdout = '{"some": "data"}'
-
-    mock = OutputMock()
-    print(mock.stdout)
-    return mock
-
-
-async def override_dependency_2(user_id: int):
-    class OutputMock:
-        stdout = '["data"]'
-
-    mock = OutputMock()
-    print(mock.stdout)
-    return mock
-
-
-async def override_dependency_failure():
-    raise HTTPException(status_code=404, detail="File Not Found")
-
-
-app.dependency_overrides[run_model_dependency] = override_dependency
-app.dependency_overrides[run_model_for_user] = override_dependency_2
+app.dependency_overrides[run_model_dependency] = mocks.run_model
+app.dependency_overrides[run_model_for_user] = mocks.run_model_for_user
 
 
 def test_index_redirects_to_docs():
@@ -46,10 +23,8 @@ def test_predict_model_success():
 
 
 def test_predict_model_failure(fastapi_dep):
-    with fastapi_dep(app).override({run_model_dependency: override_dependency_failure}):
+    with fastapi_dep(app).override({run_model_dependency: mocks.run_model_error}):
         response = client.get("/predict/model/base")
-        print(response.headers, response.url, response.json())
-
         assert response.status_code == 404
 
 
