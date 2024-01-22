1c9bbca14c9ce0b2702cd49016e277ec00673fce
Mewash1 <mizak.milosz@gmail.com>
2024-01-22 22:38:33 +0100

    finishing touches on compare.py and final docs

--- src/models/compare.py
+++ src/models/compare.py
@@ -1,5 +1,6 @@
 from src.models.base_model import BaseModel
 from src.models.advanced_model import AdvancedModel
+from src.domain.Playlist import Playlist
 import os
 import pandas as pd
 
@@ -16,6 +17,56 @@ POPULARITY_PATH = "file://" + os.path.abspath(
 )
 
 
+def get_model_results(playlists: list[Playlist]) -> dict[str, float]:
+    results = {
+                "size": 0,
+                "min_track_popularity": 0,
+                "max_track_popularity": 0,
+                "mean_popularity": 0,
+                "retention": 0
+    }
+
+    for playlist in playlists:
+        results["size"] += playlist.size
+        results["min_track_popularity"] += playlist.min
+        results["max_track_popularity"] += playlist.max
+        results["mean_popularity"] += playlist.mean
+        results["retention"] += playlist.get_retention()
+
+    results["size"] = results["size"] / len(playlists)
+    results["min_track_popularity"] = results["min_track_popularity"] / len(playlists)
+    results["max_track_popularity"] = results["max_track_popularity"] / len(playlists)
+    results["mean_popularity"] = results["mean_popularity"] / len(playlists)
+    results["retention"] = results["retention"] / len(playlists)
+
+    return results
+
+
+def compress_dicts(dicts: list[dict[str, float]]) -> dict[str, float]:
+    results = {
+                "size": 0,
+                "min_track_popularity": 0,
+                "max_track_popularity": 0,
+                "mean_popularity": 0,
+                "retention": 0
+    }
+
+    for playlist in dicts:
+        results["size"] += playlist["size"]
+        results["min_track_popularity"] += playlist["min_track_popularity"]
+        results["max_track_popularity"] += playlist["max_track_popularity"]
+        results["mean_popularity"] += playlist["mean_popularity"]
+        results["retention"] += playlist["retention"]
+
+    results["size"] = results["size"] / len(dicts)
+    results["min_track_popularity"] = results["min_track_popularity"] / len(dicts)
+    results["max_track_popularity"] = results["max_track_popularity"] / len(dicts)
+    results["mean_popularity"] = results["mean_popularity"] / len(dicts)
+    results["retention"] = results["retention"] / len(dicts)
+
+    return results
+
+
 def compare():
     stats_data = pd.read_json(STATS_PATH, lines=True)
     popularity_data = pd.read_json(POPULARITY_PATH, lines=True)
@@ -24,42 +75,26 @@ def compare():
     )
 
     track_limit = 10
+    iterations = 10
 
     advanced_model = AdvancedModel(track_limit_per_playlist=track_limit)
     base_model = BaseModel(track_limit_per_playlist=track_limit)
+    overall_results = dict()
+    overall_results["base"] = list()
+    overall_results["advanced"] = list()
 
-    base_playlists = base_model.predict(popularity_data)
-    advanced_playlists = advanced_model.predict(corrected_data)
+    for _ in range(iterations):
+        base_playlists = base_model.predict(popularity_data)
+        advanced_playlists = advanced_model.predict(corrected_data)
 
-    base_results = []
-    for _, playlist in base_playlists.items():
-        base_results.append(
-            {
-                "size": playlist.size,
-                "min_track_popularity": playlist.min,
-                "max_track_popularity": playlist.max,
-                "mean_popularity": f"{playlist.mean:.2f}",
-                "tracks": [track.to_json_serializable() for track in playlist.tracks],
-                "retention": playlist.get_retention()
-            }
-        )
+        overall_results["base"].append(get_model_results(list(base_playlists.values())))
+        overall_results["advanced"].append(get_model_results(advanced_playlists))
 
-    advanced_results = []
-    for playlist in advanced_playlists:
-        advanced_results.append(
-            {
-                "size": playlist.size,
-                "min_track_popularity": playlist.min,
-                "max_track_popularity": playlist.max,
-                "mean_popularity": f"{playlist.mean:.2f}",
-                "tracks": [track.to_json_serializable() for track in playlist.tracks],
-                "retention": playlist.get_retention()
-            }
-        )
+    overall_results["base"] = compress_dicts(overall_results["base"])
+    overall_results["advanced"] = compress_dicts(overall_results["advanced"])
 
-    print(base_results)
-    print(advanced_results)
+    return overall_results
 
 
 if __name__ == "__main__":
-    compare()
+    print(compare())
